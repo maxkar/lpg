110edf6e983076b807e8f1494c9f4ef1c6849d8f
maxkar <Maxim.Karvonen@gmail.com>
2014-02-28 19:34:01 +0400

    * Removed remains of an old writer

--- backend/js/src/ru/maxkar/backend/js/out/BadLabelException.scala
+++
@@ -1,5 +0,0 @@
-package ru.maxkar.backend.js.out
-
-final class BadLabelException(v : AnyRef) extends
-  Exception("Reference to invalid local label " + v)
-
--- backend/js/src/ru/maxkar/backend/js/out/BadVariableException.scala
+++
@@ -1,4 +0,0 @@
-package ru.maxkar.backend.js.out
-
-final class BadVariableException(v : AnyRef) extends
-  Exception("Reference to invalid local variable " + v)
